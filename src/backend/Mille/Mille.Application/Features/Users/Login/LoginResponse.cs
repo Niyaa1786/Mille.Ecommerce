9d3831805c18abcd8f95732b0aec745ba809acd3
Niyaa1786 <j4code1786@gmail.com>
2026-08-16 17:29:30 +0700

    feat(auth): implement user login use case

---
+++ src/backend/Mille/Mille.Application/Features/Users/Login/LoginResponse.cs
@@ -0,0 +1,16 @@
+﻿using Mille.Application.Common.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mille.Application.Features.Users.Login
+{
+    public class LoginResponse
+    {
+        public string AccessToken { get; set; } = string.Empty;
+        public string RefreshToken { get; set; } = string.Empty;
+        public DateTime AccessTokenExpiration { get; set; }
+        public DateTime RefreshTokenExpiration { get; set; }
+        public UserDto? User { get; set; }
+    }
+}
