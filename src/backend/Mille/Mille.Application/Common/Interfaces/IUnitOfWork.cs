fd531db384d34639b2c4c6dd6c1fb96c263a060a
Niyaa1786 <j4code1786@gmail.com>
2026-08-16 16:18:05 +0700

    feat(data): add UnitOfWork, AppDbContext and UserRepository

---
+++ src/backend/Mille/Mille.Application/Common/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using Mille.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mille.Application.Common.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IUserRepository Users { get; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ src/backend/Mille/Mille.Infrastructure/Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using Mille.Application.Common.Interfaces;
+using Mille.Domain.Interfaces;
+using Mille.Infrastructure.Persistence.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mille.Infrastructure.Persistence.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        private IUserRepository _userRepository;
+
+        public UnitOfWork(AppDbContext context) => _context = context;
+
+        public IUserRepository Users => _userRepository ??= new UserRepository(_context);
+
+        public Task<int> SaveChangesAsync(CancellationToken ct = default) => _context.SaveChangesAsync(ct);
+    }
+}
