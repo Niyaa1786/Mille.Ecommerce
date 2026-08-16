779552beda9982103fbc86109b51ba9f8ab19841
Niyaa1786 <j4code1786@gmail.com>
2026-08-16 17:13:37 +0700

    refactor(exceptions): rename ValidationException to AppValidationException to resolve ambiguity

---
+++ src/backend/Mille/Mille.Application/Common/Exceptions/AppValidationException.cs
@@ -0,0 +1,14 @@
+﻿
+using FluentValidation;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mille.Application.Common.Exceptions
+{
+    public class AppValidationException : FluentValidation.ValidationException
+    {
+        public AppValidationException(string propertyName, string errorMessage) : base(new[] { new ValidationFailure(propertyName, errorMessage) }) { }
+    }
+}
