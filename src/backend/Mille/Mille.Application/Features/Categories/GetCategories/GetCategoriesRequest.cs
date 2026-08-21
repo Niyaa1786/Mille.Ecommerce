a610d0186cefd49c693446ae4e8f54bd98cd5c5d
Niyaa1786 <j4code1786@gmail.com>
2026-08-21 18:08:03 +0700

    feat: implement get categories use case

---
+++ src/backend/Mille/Mille.Application/Features/Categories/GetCategories/GetCategoriesRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mille.Application.Features.Categories.GetCategories
+{
+    public class GetCategoriesRequest
+    {
+        public bool IncludeDeleted { get; set; } = false;
+        public int Page {  get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+        public string? Keyword { get; set; }
+    }
+}
---
+++ src/backend/Mille/Mille.Application/Features/Categories/GetCategories/GetCategoriesUseCase.cs
@@ -0,0 +1,46 @@
+﻿using Mille.Application.Common.DTOs;
+using Mille.Application.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mille.Application.Features.Categories.GetCategories
+{
+    public class GetCategoriesUseCase : IUseCase<GetCategoriesRequest, GetCategoriesResponse>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public GetCategoriesUseCase(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<GetCategoriesResponse> ExecuteAsync(GetCategoriesRequest request, CancellationToken ct = default)
+        {
+            var categories = await _unitOfWork.Categories.GetCategoriesAsync(
+                request.Keyword,
+                request.IncludeDeleted,
+                request.Page,
+                request.PageSize,
+                ct);
+
+            var totalCount = await _unitOfWork.Categories.CountAsync(request.IncludeDeleted, ct);
+            var items = categories.Select(c => new CategoryDto
+            {
+                Id = c.Id,
+                Name = c.Name,
+                Description = c.Description,
+                CreatedAt = c.CreatedAt,
+                UpdatedAt = c.UpdatedAt,
+            });
+
+
+            return new GetCategoriesResponse
+            {
+                Items = items.ToList(),
+                Page = request.Page,
+                PageSize = request.PageSize,
+                TotalCount = totalCount
+            };
+        }
+    }
+}
