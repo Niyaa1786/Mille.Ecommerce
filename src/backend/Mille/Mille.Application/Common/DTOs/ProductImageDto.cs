169234475f596a89ac12fc194f0c7ac1cae7ae96
Niyaa1786 <j4code1786@gmail.com>
2026-08-23 14:47:13 +0700

    fix: remove property VariantId replace with string PublicId

--- src/backend/Mille/Mille.Application/Common/DTOs/ProductImageDto.cs
+++ src/backend/Mille/Mille.Application/Common/DTOs/ProductImageDto.cs
@@ -7,8 +7,8 @@ namespace Mille.Application.Common.DTOs
     public class ProductImageDto
     {
         public int Id { get; set; }
+        public string PublicId { get; set; } = string.Empty;
         public string ImageUrl { get; set; } = string.Empty;
         public bool IsThumbnail { get; set; }
-        public Guid? ProductVariantId { get; set; }
     }
 }
