a40998e7136d7a73d95d5c039738472cc6998269
Niyaa1786 <j4code1786@gmail.com>
2026-08-26 16:48:47 +0700

    feat: add Cart and CartItem entities

---
+++ src/backend/Mille/Mille.Domain/Entities/Cart.cs
@@ -0,0 +1,70 @@
+﻿using Mille.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mille.Domain.Entities
+{
+    public class Cart
+    {
+        public Guid Id { get; private set; }
+        public Guid UserId { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public DateTime UpdatedAt { get; private set; }
+
+        private readonly List<CartItem> _items = new();
+        public IReadOnlyCollection<CartItem> Items => _items.AsReadOnly();
+
+        public User User { get; private set; }
+
+        private Cart() { }
+
+        public Cart(Guid userId)
+        {
+            Id = Guid.NewGuid();
+            UserId = userId;
+            CreatedAt = DateTime.UtcNow;
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        public CartItem AddItem(ProductVariant variant, int quantity)
+        {
+            var existingItem = _items.FirstOrDefault(ci => ci.ProductVariantId == variant.Id);
+            if(existingItem == null)
+            {
+                var newItem = new CartItem(this.Id, variant.Id, quantity);
+                _items.Add(newItem);
+
+                UpdatedAt = DateTime.UtcNow;
+                return newItem;
+            }
+            else
+            {
+                existingItem.IncreaseQuantity(quantity);
+
+                UpdatedAt = DateTime.UtcNow;
+                return existingItem;
+            }
+        }
+
+        public void RemoveItem(Guid cartId)
+        {
+            var item = _items.FirstOrDefault(ci => ci.CartId == cartId);
+            if (item == null)
+                throw new DomainException("Cart Item not found");
+
+            _items.Remove(item);
+            UpdatedAt = DateTime.UtcNow;
+        }
+
+        public void SetItemQuantity(int cartItemId, int newQuantity)
+        {
+            var item = _items.FirstOrDefault(ci => ci.Id ==  cartItemId);
+            if(item == null)
+                throw new DomainException("Cart Item not found");
+
+            item.SetQuantity(newQuantity);
+            UpdatedAt = DateTime.UtcNow;
+        }
+    }
+}
---
+++ src/backend/Mille/Mille.Domain/Entities/CartItem.cs
@@ -0,0 +1,53 @@
+﻿using Mille.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mille.Domain.Entities
+{
+    public class CartItem
+    {
+        public int Id { get; private set; }
+        public Guid CartId { get; private set; }
+        public Guid ProductVariantId { get; private set; }
+        public int Quantity { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+
+        public Cart Cart { get; private set; }
+        public ProductVariant ProductVariant { get; private set; }
+
+        private CartItem() { }
+
+        public CartItem(Guid cartId, Guid variantId, int quantity)
+        {
+            CartId = cartId;
+            ProductVariantId = variantId;
+            Quantity = quantity;
+            CreatedAt = DateTime.UtcNow;
+        }
+
+        public void SetQuantity(int newQuantity)
+        {
+            if(newQuantity <= 0)
+                throw new DomainException("Amount must be positive");
+            Quantity = newQuantity;
+        }
+
+        public void IncreaseQuantity(int amount)
+        {
+            if (amount <= 0)
+                throw new DomainException("Amount must be positive");
+            Quantity += amount;
+        }
+
+        public void DecreaseQuantity(int amount)
+        {
+            if (amount <= 0)
+                throw new DomainException("Amount must be positive");
+            if(Quantity - amount <= 0)
+                throw new DomainException("Quantity cannot be reduced below 1.");
+
+            Quantity -= amount;
+        }
+    }
+}
