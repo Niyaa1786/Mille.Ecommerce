1920bb550f5feb2c74321528cdadac38215c75f9
Niyaa1786 <j4code1786@gmail.com>
2026-09-07 22:19:56 +0700

    feat: change global primary color

--- src/frontend/mille-ecommerce/src/main.ts
+++ src/frontend/mille-ecommerce/src/main.ts
@@ -6,11 +6,30 @@ import router from './router'
 import PrimeVue from 'primevue/config'
 import Aura from '@primeuix/themes/aura'
 import { ConfirmationService, ToastService } from 'primevue'
+import { definePreset, type Preset } from '@primeuix/themes'
+
+const MinimalistIndigoPreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '#eep2ff',
+      100: '#e0e7ff',
+      200: '#c7d2fe',
+      300: '#a5b4fc',
+      400: '#818cf8',
+      500: '#6366f1',
+      600: '#4f46e5',
+      700: '#4338ca',
+      800: '#3730a3',
+      900: '#312e81',
+      950: '#1e1b4b',
+    },
+  },
+})
 
 const app = createApp(App)
 app.use(PrimeVue, {
   theme: {
-    preset: Aura,
+    preset: MinimalistIndigoPreset,
     options: {
       darkModeSelector: 'none',
       cssLayer: {
