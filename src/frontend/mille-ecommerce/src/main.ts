e73e4f37c062769885755cadcc7dcc2047dce341
Niyaa1786 <j4code1786@gmail.com>
2026-09-07 20:40:24 +0700

    feat: install and implement primevue and its libraries

--- src/frontend/mille-ecommerce/src/main.ts
+++ src/frontend/mille-ecommerce/src/main.ts
@@ -3,8 +3,22 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
 
 const app = createApp(App)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+    options: {
+      darkModeSelector: 'none',
+      cssLayer: {
+        name: 'primevue',
+        order: 'theme, base, primevue',
+      },
+    },
+  },
+})
 
 app.use(createPinia())
 app.use(router)
