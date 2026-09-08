a517f4a4d26673d9dcd13184e73bbb02513bc594
Niyaa1786 <j4code1786@gmail.com>
2026-09-08 15:23:48 +0700

    feat: implement AdminLayout

---
+++ src/frontend/mille-ecommerce/src/shared/components/NavLinks.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+export interface NavItem {
+  label: string
+  to: string
+  icon?: string
+}
+
+defineProps<{
+  title?: string
+  items: NavItem[]
+}>()
+</script>
+
+<template>
+  <SidebarGroup>
+    <SidebarGroupContent>
+      <SidebarMenu>
+        <SidebarMenuItem v-for="item in items">
+          <SidebarMenuButton>
+            <RouterLink :to="item.to" class="font-medium">{{ item.label }}</RouterLink>
+          </SidebarMenuButton>
+        </SidebarMenuItem>
+      </SidebarMenu>
+    </SidebarGroupContent>
+  </SidebarGroup>
+</template>
---
+++ src/frontend/mille-ecommerce/src/shared/layouts/AdminLayout.vue
@@ -0,0 +1,40 @@
+<template>
+  <SidebarLayout>
+    <Sidebar id="nav" collapsible="none">
+      <SidebarSpacer />
+      <SidebarAside>
+        <SidebarPanel>
+          <SidebarHeader class="border-b-2 border-surface-200">
+            <span class="truncate font-bold text-slate-900 text-center"> Mille Admin </span>
+          </SidebarHeader>
+
+          <SidebarContent>
+            <NavLinks :items="navItems"></NavLinks>
+          </SidebarContent>
+
+          <SidebarFooter class="border-t-2 border-surface-200">
+            <Button @click="handleLogout" severity="danger" variant="outlined" fluid>Logout</Button>
+          </SidebarFooter>
+        </SidebarPanel>
+      </SidebarAside>
+    </Sidebar>
+
+    <SidebarMain>
+      <slot></slot>
+    </SidebarMain>
+  </SidebarLayout>
+</template>
+
+<script setup lang="ts">
+import { useLogout } from '@/modules/auth/composables/useLogout'
+import NavLinks, { type NavItem } from '../components/NavLinks.vue'
+
+const { handleLogout } = useLogout()
+const navItems: NavItem[] = [
+  { label: 'Category', to: '/category' },
+  { label: 'Product', to: '/inbox' },
+  { label: 'Order', to: '/notifications' },
+]
+</script>
+
+<style scoped></style>
